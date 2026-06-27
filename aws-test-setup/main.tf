1cbbbac642037e314caa53623e03acd9109ff9c5
Test User <test@example.com>
2026-06-27 13:08:56 +0200

    feat: real docker build with ECR push

--- aws-test-setup/main.tf
+++ aws-test-setup/main.tf
@@ -29,14 +29,41 @@ resource "aws_iam_role_policy" "codebuild_policy" {
         Effect   = "Allow"
         Action   = ["logs:*", "s3:*"]
         Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:GetRepositoryPolicy",
+          "ecr:DescribeRepositories",
+          "ecr:ListImages",
+          "ecr:DescribeImages",
+          "ecr:BatchGetImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:PutImage"
+        ]
+        Resource = "*"
       }
     ]
   })
 }
 
-resource "aws_codebuild_project" "gitversion_test" {
-  name          = "GitVersionFullCycleTest"
-  description   = "Testing GitVersion across branches"
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+resource "aws_ecr_repository" "test_repo" {
+  name                 = "gitversion-test-app"
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+}
+
+resource "aws_codebuild_project" "gitversion_test_tag" {
+  name          = "GitVersionTestTag"
+  description   = "Testing GitVersion tagging pipeline"
   build_timeout = "10"
   service_role  = aws_iam_role.codebuild_role.arn
 
@@ -56,6 +83,45 @@ resource "aws_codebuild_project" "gitversion_test" {
     type            = "GITHUB"
     location        = "https://github.com/levin-dmytro/gitversion.git"
     git_clone_depth = 0
-    buildspec       = "ci-cd-examples/aws-codebuild/buildspec.yml"
+    buildspec       = "ci-cd-examples/aws-codebuild/buildspec-tag.yml"
+  }
+}
+
+resource "aws_codebuild_project" "gitversion_test_build" {
+  name          = "GitVersionTestBuild"
+  description   = "Testing GitVersion docker build pipeline"
+  build_timeout = "10"
+  service_role  = aws_iam_role.codebuild_role.arn
+
+  artifacts {
+    type = "NO_ARTIFACTS"
+  }
+
+  environment {
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    image                       = "aws/codebuild/standard:7.0"
+    type                        = "LINUX_CONTAINER"
+    image_pull_credentials_type = "CODEBUILD"
+    privileged_mode             = true
+
+    environment_variable {
+      name  = "AWS_ACCOUNT_ID"
+      value = data.aws_caller_identity.current.account_id
+    }
+    environment_variable {
+      name  = "AWS_DEFAULT_REGION"
+      value = data.aws_region.current.name
+    }
+    environment_variable {
+      name  = "IMAGE_REPO_NAME"
+      value = aws_ecr_repository.test_repo.name
+    }
+  }
+
+  source {
+    type            = "GITHUB"
+    location        = "https://github.com/levin-dmytro/gitversion.git"
+    git_clone_depth = 0
+    buildspec       = "ci-cd-examples/aws-codebuild/buildspec-build.yml"
   }
 }
